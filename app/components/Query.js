5cc3d567b42e1b18cc54c8513adc2dca277f3030
rukaroa <rukaroa@gmail.com>
2017-06-08 18:32:54 -0500

    query string

--- app/components/Query.js
+++ app/components/Query.js
@@ -1,10 +1,13 @@
 //need NYT API for query
 var axios = require("axios");
-var nytAPI = "";
+var nytAPI = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
 var query = {
     runQuery: function(term, startYear, endYear) {
-        var queryURL = "";
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+        queryURL += "&q=" + term;
+        queryURL += "&begin_date=" + startYear;
+        queryURL += "&end_date=" + endYear;
 
         return axious.get(queryURL).then(function(resp){
             // if success
