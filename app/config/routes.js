ff2e3b84334e29b1cfcd6519519f0ea2b3280762
Rukaroa <rukaroa@gmail.com>
2017-06-08 15:59:05 -0500

    results route added

--- app/config/routes.js
+++ app/config/routes.js
@@ -11,13 +11,15 @@ var IndexRoute = route.IndexRoute;
 var Main = require("../components/Main");
 var Saved = require("../components/Saved");
 var Search = require("../components/Search");
+var Results = require("../components/Results");
 
 module.exports = (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
             <Route path="search" component={Search}/>
             <Route path="saved" component={Saved}/>
-
+            <Route path="results" component={Results}/>
+            
             <IndexRoute component={Search}/>
         </Route>
     </Router>
