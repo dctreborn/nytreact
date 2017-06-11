c6e65b243e04ec784af9809e982aa40c0cbe69a0
Rukaroa <rukaroa@gmail.com>
2017-06-10 23:33:57 -0500

    initial render

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,19 +2,46 @@
 var React = require("react");
 
 var Results = React.createClass({
+    // renderResults: function(){
+    //     let list = this.props.results;
+    //     list.forEach(function(element) {
+    //         <li>element.title</li>
+    //         <li>element.date</li>
+    //         <li>element.url</li>
+    //     });
+    // },
     render: function(){
-        return(
-            <div className="row">
-                <div className="panel panel-default text-center">
-                    <div className="panel-heading">
-                        <div className="panel-title">Results</div>                        
-                    </div>
-                    <div className="panel-body">
-                        {this.props.results}
+        if (this.props.results != null){
+            return(
+                <div className="row">
+                    <div className="panel panel-default text-center">
+                        <div className="panel-heading">
+                            <div className="panel-title">Results</div>                        
+                        </div>
+                        <div className="panel-body">
+                            <ul>
+                                <li>{this.props.results.title}</li>
+                                <li>{this.props.results.date}</li>
+                                <li>{this.props.results.url}</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (<
+                div className="row">
+                    <div className="panel panel-default text-center">
+                        <div className="panel-heading">
+                            <div className="panel-title">Results</div>                        
+                        </div>
+                        <div className="panel-body">
+
+                        </div>
+                    </div>
+                </div>
+                );
+        }
     }
 });
 
